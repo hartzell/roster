ff29c8b56e7558a16ab5db2ecb7d4f3ef457bb45
George Hartzell <georgewh@gene.com>
2015-10-22 11:22:34 -0700

    Clean up state file handling
    Seems cleaner and might be more testable, but really there's not much
    there to test.
    
    Perhaps that it's opening the file that I think it's opening?  Otherwise
    all the beef's in terraform.ReadState().

--- state.go
+++ state.go
@@ -2,27 +2,35 @@ package main
 
 import (
 	"errors"
-	"flag"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/hashicorp/terraform/terraform"
 )
 
 func fetchState(dir string) (*terraform.State, error) {
-	file := "terraform.tfstate"
-	if flag.Arg(0) != "" {
-		file = flag.Arg(0)
+	src, err := openState(dir)
+	if err != nil {
+		return nil, err
 	}
+	defer src.Close()
+
+	state, err := terraform.ReadState(src)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Unable to read state from src"))
+	}
+	return state, nil
+}
+
+func openState(dir string) (io.ReadCloser, error) {
+	file := filepath.Join(dir, "terraform.tfstate")
+	spew.Dump(file)
 	f, err := os.Open(file)
 	if err != nil {
 		return nil, errors.New(fmt.Sprintf("Unable to open '%s'", file))
 	}
-	defer f.Close()
-
-	state, err := terraform.ReadState(f)
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("Unable to read state from '%s'", file))
-	}
-	return state, nil
+	return f, nil
 }
