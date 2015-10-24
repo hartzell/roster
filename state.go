ae84cb9ebc9885c77bdeec8c1c22afd2fbcde0c4
George Hartzell <georgewh@gene.com>
2015-10-24 14:20:35 -0700

    Add some docs to state.go

--- state.go
+++ state.go
@@ -10,6 +10,9 @@ import (
 	"github.com/hashicorp/terraform/terraform"
 )
 
+// Function fetchState takes a directory name (a string) and returns
+// an instance of *terraform.State based on what it finds in that
+// directory and an error.
 func fetchState(dir string) (*terraform.State, error) {
 	src, err := openState(dir)
 	if err != nil {
@@ -24,6 +27,8 @@ func fetchState(dir string) (*terraform.State, error) {
 	return state, nil
 }
 
+// Function openState takes a directory string and returns an open
+// file for the terraform.tfstate file in that directory and an error.
 func openState(dir string) (io.ReadCloser, error) {
 	file := filepath.Join(dir, "terraform.tfstate")
 	f, err := os.Open(file)
